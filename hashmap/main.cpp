1e4b1ea1e78f759c38b2061f33b385d7c217a0b1
msaaddev <mrsaadirfan@gmail.com>
2019-11-08 16:18:40 +0500

    📦 NEW: hashkey

---
+++ hashmap/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int hashkey(int data)
+{
+    if (data % 2 != 0)
+    {
+        return 1
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int main()
+{
+    
+}
